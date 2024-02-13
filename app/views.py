40eb841a89fb11cb3a0f29d15d45180ea95b632d
DavidLynch26 <lynchda78@gmail.com>
2024-02-13 17:48:11 -0500

    Added all body components, css styling needed

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
+import datetime
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
 
 ###
 # Routing for your application.
@@ -11,12 +11,18 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
-
 @app.route('/about/')
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route("/profile")
+def profile():
+    date = format_date_joined(datetime.datetime.now())
+    return render_template('profile.html', date = date)
+
+def format_date_joined(date):
+    return f'Joined {date.strftime("%b")}, {date.strftime("%Y")}'
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -39,7 +45,6 @@ def add_header(response):
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
-
 
 @app.errorhandler(404)
 def page_not_found(error):
